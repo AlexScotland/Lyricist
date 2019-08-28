64f530f5e8af3e576dff999f33bd0df3c21cef0a
AlexScotland <root@dev-environment.us-central1-a.c.scotland-project.internal>
2019-08-28 02:33:03 +0000

    Dataset push :)

--- get_artists.py
+++ get_artists.py
@@ -1,10 +1,134 @@
-import urllib.request, requests, sys, os
+import urllib.request, requests, sys, os, time
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urlencode, quote_plus
 import http.client as http_client4
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
+class htmlStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+class webSession:
+    def __init__(self):
+        self.session = requests.Session()
+        self.artists = []
+        self.proxies = {
+        "https":"93.125.121.98:8081",
+        "http":"167.71.176.202:8080",
+            }
+
+    def getArtists(self,url):
+        page = self.session.get(url)
+        self.scraper = BeautifulSoup(page.text,'html.parser')
+        for i in self.scraper.find_all('div',{'class':'div-col columns column-width'}):
+            list_descendants = i.descendants
+            for artist in list_descendants:
+                if artist.name == 'a':
+                    if artist.text == '[1]':
+                        pass
+                    elif artist.text == '[2]':
+                        pass
+                    else:
+                        self.artists.append(artist.text)
+
+    def writeArtistsToFile(self):
+        with open("artist.txt", "w") as txtFile:
+            for i in self.artists:
+                try:
+                    txtFile.write(i+'\n')
+                except:
+                    print('Error importing')
+                finally:
+                    pass
+            txtFile.close()
+
+    def stripTags(html):
+        s = htmlStripper()
+        s.feed(html)
+        return s.get_data()
+
+    def concatURL(self,artist_name):
+        if not artist_name[0].isalpha():
+            artist_name=artist_name.lower()
+            first_letter = '19'
+        else:
+            artist_name=artist_name.lower()
+            first_letter = artist_name[0]
+        url = "http://www.azlyrics.com/"+first_letter+"/"+artist_name.replace(" ", "")+".html"
+        return url
+
+    def getListFromFile(self):
+        with open("artist.txt", "r") as txtFile:
+            for i in txtFile:
+                try:
+                    i= i.replace('\n','')
+                    i = i.replace('/','')
+                    i= i.replace(':','')
+                    self.artists.append(i)
+                except:
+                    print('Error importing')
+                finally:
+                    pass
+            txtFile.close()
+
+
+    def getSongList(self,url):
+        song_list = []
+        page = self.session.get(url)
+        soup = BeautifulSoup(page.text,"html.parser")
+        for song in soup.find_all('div',{'id':'listAlbum'}):
+            for link in song.find_all('a'):
+                song_link=link.get('href')
+                song_name=link.get_text()
+                song_link=song_link[3:]
+                generated_link='http://www.azlyrics.com/'+song_link
+                song_list.append((song_name,generated_link))
+        return song_list
+
+    def getLyrics(self,song_url):
+        try:
+            page=self.session.get(song[1])
+        except Exception as msg:
+            print("[ERR ] Cannot Navigate to link:  "+str(msg))
+            print("[Cont] changing VPN settings and waiting 10 seconds")
+            os.system('pypia -s')
+            time.sleep(10)
+        else:
+            soup=BeautifulSoup(page.text,'lxml')
+            soup=soup.find_all('div', class_="")
+            text=soup[1]
+            text=str(text)
+            text=removeHTML(text)
+            text=text.replace("<!-- Usage of azlyrics.com text by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->","")
+            return text
+
+def removeHTML(innerHTML):
+    started=False
+    for i in innerHTML:
+        if i == '<':
+            started=True
+            temp_string = ''
+        if started:
+            temp_string+= i
+        if i == '>':
+            started=False
+            innerHTML=innerHTML.replace(temp_string,'')
+    return innerHTML
+
+def writeTextFile(songText, songName):
+    with open("dataset.txt", "a") as txtFile:
+        txtFile.write(songText)
+        txtFile.close()
 
 hello = webSession()
 ### GET THE RAPPERS MAIYNNE
@@ -20,12 +144,12 @@ except Exception as msg:
         hello.getArtists('https://en.wikipedia.org/wiki/List_of_hip_hop_musicians')
         hello.writeArtistsToFile()
 finally:
-    print(hello.artists)
+    pass
 
 #### LETS START SCRAPING, MAYN
 
 for name in hello.artists:
-   try:
+    try:
         artist_url = hello.concatURL(name)
         song_list = hello.getSongList(artist_url)
         counter = 0
@@ -36,8 +160,9 @@ for name in hello.artists:
             else:
                 writeTextFile(song_lyrics, str(song_list[counter][0]))
                 counter +=1
-   except Exception as err:
-       print('[ERR ]  Artist could not be parsed.')
-       print(str(err))
-   finally:
-       pass
+    except Exception as err:
+        print('[ERR ]  Artist could not be parsed.')
+        print(str(err))
+        print(name)
+    finally:
+        pass
